ad33cbcfcdf392b83741f975e8a7226f95f47229
Joar Classon <joar@classon.se>
2015-05-07 10:09:04 +0200

    increased size of login inputs and buttons

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
             <input type="hidden" name="action" value="login">
             <div class="login-form">
               <label for="username">Username and Password: </label>
-              <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
-              <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
-              <button type="submit" class="btn btn-primary btn-block" id="loginButton">Login</button>
+              <input type="text" name="username" id="username" class="form-control input-lg" placeholder="Username" required autofocus>
+              <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" required>
+              <button type="submit" class="btn btn-primary btn-block btn-lg" id="loginButton">Login</button>
             </div>
           </form>
         </div>
