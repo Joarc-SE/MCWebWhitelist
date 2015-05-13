276d537dedfa172ff6da5eb59c5f40ba279ed55e
Joar Classon <joar@classon.se>
2015-05-13 16:17:18 +0200

    stuff

--- index.main.php
+++ index.main.php
@@ -5,7 +5,7 @@
   <div role="tabpanel">
 
     <!-- Nav tabs -->
-    <ul class="nav nav-tabs" role="tablist">
+    <ul class="nav nav-tabs" role="tablist" id="tabs">
       <li role="presentation" class="active"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
       <li role="presentation"><a href="#whitelist" aria-controls="whitelist" role="tab" data-toggle="tab">Whitelist</a></li>
       <li role="presentation"><a href="#users" aria-controls="users" role="tab" data-toggle="tab">Users</a></li>
@@ -15,6 +15,9 @@
     <!-- Tab panes -->
     <div class="tab-content">
       <div role="tabpanel" class="tab-pane active" id="profile">
+        <p>Profile</p>
+      </div>
+      <div role="tabpanel" class="tab-pane" id="whitelist">
         <table class="table table-bordered">
           <thead>
             <tr>
@@ -44,9 +47,6 @@
           </tbody>
         </table>
       </div>
-      <div role="tabpanel" class="tab-pane" id="whitelist">
-        <p>Whitelist</p>
-      </div>
       <div role="tabpanel" class="tab-pane" id="users">
         <p>Users</p>
       </div>
@@ -57,10 +57,11 @@
   </div>
 </div>
 <script>
-$('#profile a').click(function (e) {
+$('#tabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+/*
 $('#whitelist a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
@@ -72,5 +73,5 @@ $('#users a').click(function (e) {
 $('#settings a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
-})
+})*/
 </script>
