91d9b02ccc43700cdb2ce0104b42764935157e79
Joar Classon <joar@classon.se>
2015-05-13 15:39:35 +0200

    updating functions

--- functions.php
+++ functions.php
@@ -17,12 +17,14 @@ function changeRank($rank) {
 }
 
 function populateDB() {
+  unlink("database.sqlite");
   $password = password_hash("admin", PASSWORD_BCRYPT);
   $db = new PDO('sqlite:database.sqlite');
   $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, rank INTEGER)');
   $db->query('CREATE TABLE whitelisted(id INTEGER PRIMARY KEY, username TEXT, addedby INTEGER, date TEXT)');
   $db->query('CREATE TABLE settings(id INTEGER PRIMARY KEY, guestrank INT, rconport INTEGER, rconpassword TEXT,queryport INTEGER)');
-  $db->query('INSERT INTO users (username, password, rank) VALUES ("admin", "$password", 4)');
+  $query = $db->prepare('INSERT INTO users (username, password, rank) VALUES ("admin", ?, 4)');
+  $query->execute(array($password));
   $db->query('INSERT INTO whitelisted (username, addedby, date) VALUES ("joarc", 1, 1)');
   $db->query('INSERT INTO settings (guestrank, rconport, rconpassword, queryport) VALUES (0, 25575, "password", 25565)');
 }
