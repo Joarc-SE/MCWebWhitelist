045bfc5336f4d0ddb1970ef17417799d47123d53
Joar Classon <joar@classon.se>
2015-05-13 15:25:07 +0200

    encrypted passwords

--- functions.php
+++ functions.php
@@ -17,11 +17,12 @@ function changeRank($rank) {
 }
 
 function populateDB() {
+  $password = password_hash("admin", PASSWORD_BCRYPT)
   $db = new PDO('sqlite:database.sqlite');
   $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, rank INTEGER)');
   $db->query('CREATE TABLE whitelisted(id INTEGER PRIMARY KEY, username TEXT, addedby INTEGER, date TEXT)');
   $db->query('CREATE TABLE settings(id INTEGER PRIMARY KEY, guestrank INT, rconport INTEGER, rconpassword TEXT,queryport INTEGER)');
-  $db->query('INSERT INTO users (username, password, rank) VALUES ("admin", "admin", 4)');
+  $db->query('INSERT INTO users (username, password, rank) VALUES ("admin", "$password", 4)');
   $db->query('INSERT INTO whitelisted (username, addedby, date) VALUES ("joarc", 1, 1)');
   $db->query('INSERT INTO settings (guestrank, rconport, rconpassword, queryport) VALUES (0, 25575, "password", 25565)');
 }
