2b288743f53b341bb2ea97e350834ef13743d071
Joar Classon <joar@classon.se>
2015-05-13 14:56:19 +0200

    even more noob

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function populateDB() {
   $db = new PDO('sqlite:database.sqlite');
   $db->query('CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, rank INTEGER)');
   $db->query('CREATE TABLE whitelisted(id INTEGER PRIMARY KEY, username TEXT, addedby INTEGER, date TEXT)');
-  $db->query('CREATE TABLE settings(id INTEGER PRIMARY KEY, guestrank INT, rconport INTEGER, rconpassword TEXT,queryport INTEGER)')
+  $db->query('CREATE TABLE settings(id INTEGER PRIMARY KEY, guestrank INT, rconport INTEGER, rconpassword TEXT,queryport INTEGER)');
   $db->query('INSERT INTO users (username, password, rank) VALUES ("admin", "admin", 4)');
   $db->query('INSERT INTO whitelisted (username, addedby, date) VALUES ("joarc", 1, 1)');
   $db->query('INSERT INTO settings (guestrank, rconport, rconpassword, queryport) VALUES (0, 25575, "password", 25565)');
